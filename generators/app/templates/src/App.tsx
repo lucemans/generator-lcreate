d1092165ec83a9e433dea6d6a8c78585ed5217ad
Lucemans <luc@lucemans.nl>
2020-10-23 15:33:57 +0200

    Added Branding and Hello World

--- generators/app/templates/src/App.tsx
+++ generators/app/templates/src/App.tsx
@@ -4,6 +4,11 @@ import { useEffect, useState } from 'react';
 export default function App(props: any) {
    
    return (
-      <div className="app"></div>
+      <div className="app">
+         <pre>
+            Welcome to the wonderfull world of <span className="blue">react</span>
+            <span className="luc">~Luc</span>
+         </pre>
+      </div>
    );
 }
